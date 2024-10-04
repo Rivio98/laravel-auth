b00fb7270b82d410c7bab3c5e5daeed686bfbd1e
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-04 15:40:19 +0200

    [UPD]Crerated and ripopolate database with seeder

--- database/migrations/2024_10_04_132118_create_projects_table.php
+++ database/migrations/2024_10_04_132118_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name, 200');
+            $table->string('name', 200);
             $table->string('slug');
             $table->string('description')->nullable();
             $table->timestamps();
